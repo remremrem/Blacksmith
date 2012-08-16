69be50352d22c6b775c87c243415006fc3c94adf
jrbudda <jrbudda@gmail.com>
2012-08-15 21:01:26 -0400

    Configurable price per durability point

--- src/main/java/net/apunch/blacksmith/util/Settings.java
+++ src/main/java/net/apunch/blacksmith/util/Settings.java
@@ -36,7 +36,7 @@ public class Settings {
 
     public enum Setting {
         BASE_PRICE("base-prices.default", 10),
-        PRICE_PER_DURABILITY_POINT("base-prices.price-per-durability-point", 1),
+        PRICE_PER_DURABILITY_POINT("price-per-durability-point.default", 1),
         BUSY_WITH_PLAYER_MESSAGE("defaults.messages.busy-with-player", "§cI'm busy at the moment. Come back later!"),
         BUSY_WITH_REFORGE_MESSAGE("defaults.messages.busy-with-reforge", "§cI'm working on it. Be patient!"),
         COOLDOWN_UNEXPIRED_MESSAGE(
